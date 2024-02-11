dbd9c8b917c77f3072e9707a9beabfcffc9411fa
JoelineAlves <joelinealves08@gmail.com>
2024-02-11 21:37:34 +0000

    generate the cards using the tag and className parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,13 @@ const signals = [
   'stop'   
 ];
 
+/**
+ * Generating the cards using the tag and className parameters
+ * @param {*} tag 
+ * @param {*} className  
+ */
+function createElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
